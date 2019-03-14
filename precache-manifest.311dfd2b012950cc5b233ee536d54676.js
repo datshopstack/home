fb4cbac6ec4c99ab3c90b825d88f683dbc2dcace
Dat Hoang <dat@shopstack.asia>
2019-03-14 13:44:48 +0700

    Updates

---
+++ precache-manifest.311dfd2b012950cc5b233ee536d54676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5fdf7f86495bc64854ab",
+    "url": "/home/static/js/runtime~main.5fdf7f86.js"
+  },
+  {
+    "revision": "5701fd3f3b0e4112482e",
+    "url": "/home/static/js/main.5701fd3f.chunk.js"
+  },
+  {
+    "revision": "06fade93b678a28291e1",
+    "url": "/home/static/js/2.06fade93.chunk.js"
+  },
+  {
+    "revision": "5701fd3f3b0e4112482e",
+    "url": "/home/static/css/main.a5d897f5.chunk.css"
+  },
+  {
+    "revision": "c005e0e443ae912d15b76b94f2f68d06",
+    "url": "/home/index.html"
+  }
+];
